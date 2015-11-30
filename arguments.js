5aa82c15965862458173309cf119296bb49aa1e3
Ben Rothman <croquetpro@gmail.com>
2015-11-30 13:53:06 -0800

    Now, done with arguments...

--- arguments.js
+++ arguments.js
@@ -34,13 +34,37 @@ var curriedSum = function(numArgs) {
     } else {
       return _curriedSum;
     }
-  };
-  return _curriedSum;
+  }
+  return _curriedSum
 };
-// Function.prototype.curry = function (numArgs) {
-//   var fn = this;
-//
-// };
 
-var sum = curriedSum(5);
-console.log(sum(5)(2)(3)(2)(0));
+Function.prototype.curry = function (numArgs) {
+  var fn = this;
+  var args = []
+  function _curry(num) {
+    args.push(num);
+    // console.log(args);
+    if (args.length === numArgs) {
+      // console.log(fn);
+      return fn.apply(undefined, args);
+    } else {
+      return _curry;
+    }
+  };
+  return _curry;
+};
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+// console.log(sumThree(4, 20, 6)); // == 30
+
+// you'll write `Function#curry`!
+// var f1 = sumThree.curry(3);
+// var f2 = f1(4);
+// var f3 = f2(20);
+// var result = f3(6); // = 30
+
+// or more briefly:
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
