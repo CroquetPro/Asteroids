52dbdbe60a0ac6c3ae722df2aa21cc25c295cbb0
Ben Rothman <croquetpro@gmail.com>
2015-11-30 14:16:42 -0800

    Done with inherits... on to asteroids

---
+++ inherits.js
@@ -0,0 +1,34 @@
+Function.prototype.inherits = function(parent) {
+  function Surrogate(){};
+  Surrogate.prototype = parent.prototype;
+  this.prototype = new Surrogate ();
+  this.prototype.constructor = this;
+};
+
+function MovingObject (name) {
+  this.name = name;
+  this.shape = "triangle";
+};
+
+MovingObject.prototype.f = function(){
+  return "Hey.." + this.name;
+}
+
+function Ship () {
+  MovingObject.call(this, "shdacssasape");
+};
+Ship.inherits(MovingObject);
+
+function Asteroid () {};
+Asteroid.inherits(MovingObject);
+
+Asteroid.prototype.crash = function () {
+  return "no more dinosour"
+};
+
+var ship = new Ship();
+// var ast =
+
+console.log(ship.f());
+console.log(ship.shape);
+console.log(ship.crash);
