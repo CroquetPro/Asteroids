d05dea03a50d7a478a2031200e23ff58e0ecea4e
Ben Rothman <croquetpro@gmail.com>
2015-11-30 14:58:25 -0800

    started movingObject

---
+++ asteroids/lib/movingObject.js
@@ -0,0 +1,35 @@
+(function(){
+  if (typeof Asteroids === "undefined" ){
+    window.Asteroids = {};
+  }
+
+function MovingObject(pos, vel, radius, color){
+  this.pos = pos;
+  this.vel = vel;
+  this.radius = radius;
+  this.color = color;
+}
+
+MovingObject.prototype.draw = function (ctx) {
+  ctx.fillstyle = this.color;
+  ctx.beginPath();
+
+  ctx.arc(
+      this.pos[0],
+      this.pos[1],
+      this.radius,
+      0,
+      2 * Math.PI,
+      false
+    );
+
+    ctx.fill();
+};
+
+MovingObject.prototype.move = function () {
+  this.pos[0] += this.(vel[0]*0.01);
+  this.pos[1] += this.vel[1]*0.01;
+};
+
+
+})()
--- inherits.js
+++ inherits.js
@@ -4,31 +4,31 @@ Function.prototype.inherits = function(parent) {
   this.prototype = new Surrogate ();
   this.prototype.constructor = this;
 };
-
-function MovingObject (name) {
-  this.name = name;
-  this.shape = "triangle";
-};
-
-MovingObject.prototype.f = function(){
-  return "Hey.." + this.name;
-}
-
-function Ship () {
-  MovingObject.call(this, "shdacssasape");
-};
-Ship.inherits(MovingObject);
-
-function Asteroid () {};
-Asteroid.inherits(MovingObject);
-
-Asteroid.prototype.crash = function () {
-  return "no more dinosour"
-};
-
-var ship = new Ship();
-// var ast =
-
-console.log(ship.f());
-console.log(ship.shape);
-console.log(ship.crash);
+//
+// function MovingObject (name) {
+//   this.name = name;
+//   this.shape = "triangle";
+// };
+//
+// MovingObject.prototype.f = function(){
+//   return "Hey.." + this.name;
+// }
+//
+// function Ship () {
+//   MovingObject.call(this, "shdacssasape");
+// };
+// Ship.inherits(MovingObject);
+//
+// function Asteroid () {};
+// Asteroid.inherits(MovingObject);
+//
+// Asteroid.prototype.crash = function () {
+//   return "no more dinosour"
+// };
+//
+// var ship = new Ship();
+// // var ast =
+//
+// console.log(ship.f());
+// console.log(ship.shape);
+// console.log(ship.crash);
